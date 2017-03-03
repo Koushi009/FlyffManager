efa1bb0fd81b92b335066c438d5d8d3886fdf7a3
klinger2709 <gaming.julian94@gmail.com>
2017-03-03 12:19:06 +0100

    Scrollanzahl berechnen eingefügt

--- app/src/main/java/com/klinger2709/klingerju/flyffmanager/MoreUpgradesFragment.java
+++ app/src/main/java/com/klinger2709/klingerju/flyffmanager/MoreUpgradesFragment.java
@@ -4,13 +4,19 @@ package com.klinger2709.klingerju.flyffmanager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -19,14 +25,19 @@ import android.widget.TextView;
 public class MoreUpgradesFragment extends Fragment {
 
     View view;
-    double[] upgradeJewelry = {1,1,0.63,0.45,0.33,0.26,0.21,0.17,0.14,0.11,0.09,0.08,0.06,0.05,0.04,0.03,0.02,0.01,0.007,0.001};
-    double[] piercingSuit = {0.8,0.5,0.2,0.05};
-    double[] upgradeWeapon = {1,1,0.7,0.5,0.4,0.3,0.2,0.1,0.05,0.02};
-    double[] piercingWeapon = {0.5,0.25,0.125,0.0625,0.0313,0.0156,0.0078,0.0039,0.002,0.001};
-    double[] elementWeapon = {1,1,0.7,0.5,0.4,0.3,0.2,0.1,0.05,0.02,0.019,0.018,0.017,0.016,0.015,0.014,0.013,0.012,0.011,0.01};
-    double[] upgradeUltimate = {0.11,0.09,0.07,0.05,0.03,0.009,0.007,0.005,0.003,0.001};
-    double[][] chances = {upgradeJewelry,piercingSuit,upgradeWeapon,piercingWeapon,elementWeapon,upgradeUltimate};
+    double[] upgradeJewelry = {1, 1, 0.63, 0.45, 0.33, 0.26, 0.21, 0.17, 0.14, 0.11, 0.09, 0.08, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01, 0.007, 0.001};
+    double[] piercingSuit = {0.8, 0.5, 0.2, 0.05};
+    double[] upgradeWeapon = {1, 1, 0.7, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.02};
+    double[] piercingWeapon = {0.5, 0.25, 0.125, 0.0625, 0.0313, 0.0156, 0.0078, 0.0039, 0.002, 0.001};
+    double[] elementWeapon = {1, 1, 0.7, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.02, 0.019, 0.018, 0.017, 0.016, 0.015, 0.014, 0.013, 0.012, 0.011, 0.01};
+    double[] upgradeUltimate = {0.11, 0.09, 0.07, 0.05, 0.03, 0.009, 0.007, 0.005, 0.003, 0.001};
+    double[][] chances = {upgradeJewelry, piercingSuit, upgradeWeapon, piercingWeapon, elementWeapon, upgradeUltimate};
     int currentSelected = 0;
+
+    TextView result;
+    EditText aktuelleStufeET;
+    EditText wunschStufeET;
+    EditText wahrscheinlichkeitET;
 
     public MoreUpgradesFragment() {
         // Required empty public constructor
@@ -37,12 +48,24 @@ public class MoreUpgradesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_more_upgrades, container, false);
+        view = inflater.inflate(R.layout.fragment_more_upgrades, container, false);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Scrollanzahl berechnen");
 
         Spinner itemSelector = (Spinner) view.findViewById(R.id.item_selector);
         final TextView chanceList = (TextView) view.findViewById(R.id.chance_list);
+        aktuelleStufeET = (EditText) view.findViewById(R.id.aktuelle_stufe);
+        wunschStufeET = (EditText) view.findViewById(R.id.wunschstufe);
+        wahrscheinlichkeitET = (EditText) view.findViewById(R.id.wahrscheinlichkeit);
+        result = (TextView) view.findViewById(R.id.ergebnis);
+        result.setText("");
+
+        ((Button) view.findViewById(R.id.calculate_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+            }
+        });
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
                 R.array.upgrading_items, android.R.layout.simple_spinner_item);
@@ -54,8 +77,8 @@ public class MoreUpgradesFragment extends Fragment {
                 currentSelected = position;
                 String chancesText = "";
                 int i = 0;
-                for(double d : chances[position]) {
-                    chancesText +=  "+" + ++i + " = " + Math.round(d*1000) / 10.0 + "%\n";
+                for (double d : chances[position]) {
+                    chancesText += "+" + ++i + " = " + Math.round(d * 1000) / 10.0 + "%\n";
                 }
                 chanceList.setText(chancesText);
             }
@@ -68,4 +91,51 @@ public class MoreUpgradesFragment extends Fragment {
         return view;
     }
 
+    public boolean checkValues() {
+        if(aktuelleStufeET.getText().toString().equals("") || wunschStufeET.getText().toString().equals("") || wahrscheinlichkeitET.getText().toString().equals("")) {
+            Toast.makeText(getActivity(), "Bitte alle Felder ausfüllen", Toast.LENGTH_SHORT).show();
+            return  false;
+        }
+        if(Integer.parseInt(aktuelleStufeET.getText().toString()) > (Integer.parseInt(wunschStufeET.getText().toString()))) {
+            Toast.makeText(getActivity(), "Negativupgrades nicht möglich", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(Integer.parseInt(aktuelleStufeET.getText().toString()) > chances[currentSelected].length || Integer.parseInt(aktuelleStufeET.getText().toString()) < 0) {
+            Toast.makeText(getActivity(), "Ungültiger Wert", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+         if(Integer.parseInt(wunschStufeET.getText().toString()) > chances[currentSelected].length ||Integer.parseInt(wunschStufeET.getText().toString()) < 1) {
+             Toast.makeText(getActivity(), "Ungültiger Wert", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void calculate() {
+        if (checkValues()) {
+            int aktuelleStufe = aktuelleStufeET.getText().toString().equals("") ? 0 : Integer.parseInt(aktuelleStufeET.getText().toString());
+            int wunschStufe = wunschStufeET.getText().toString().equals("") ? 0 : Integer.parseInt(wunschStufeET.getText().toString());
+            int wahrscheinlichkeit = wahrscheinlichkeitET.getText().toString().equals("") ? 0 : Integer.parseInt(wahrscheinlichkeitET.getText().toString());
+
+
+            int gesamtZahlScrolls = 0;
+            for (int i = aktuelleStufe; i < wunschStufe; i++) {
+                double toLogA = wahrscheinlichkeit / -100f;
+                double logA = Math.log10(toLogA + 1); //-0.6989700433
+                double logB = Math.log10(1 - chances[currentSelected][i]); // -0.01322826573
+                double res = round(logA) / round(logB);
+                long scrolls = Math.round(res);
+                gesamtZahlScrolls += scrolls;
+            }
+
+            TextView ergebnisTV = (TextView) view.findViewById(R.id.ergebnis);
+            ergebnisTV.setText("Du wirst ca. " + gesamtZahlScrolls + " Scrolls brauchen.");
+        }
+    }
+
+    public double round(double doubleToRound) {
+        return Math.round(doubleToRound * 100000) / 100000.0;
+    }
 }
